4f1d5d50b5989f92dc9448355fa292150f110697
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-02-08 20:01:56 -0600

    Encapsular lógica en módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
+import { ProductsService } from './products/services/products.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
   providers: [AppService, ProductsService],
 })
 export class AppModule {}
